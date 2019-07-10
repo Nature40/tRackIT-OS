6aea8d4c7bf6eb96aab9c23b90a172c0633d3886
Jonas Höchst <git@jonashoechst.de>
2019-07-10 20:05:48 +0200

    revisited IPv4 autoconfig

--- Mesh/etc/network/interfaces.d/adhoc.sh
+++ Mesh/etc/network/interfaces.d/adhoc.sh
@@ -2,7 +2,10 @@
 
 ID=`cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2`
 
-IP=169.254.$((16#${ID:12:2})).$((16#${ID:14:2}))/16
+IP1=$((16#${ID:8:2}))
+IP2=$((16#${ID:10:2}))
+IP3=$((16#${ID:12:2}))
+IP4=$((16#${ID:14:2}))
 
 echo "Generating adhoc config..." 1>&2
 echo "Address: $IP" 1>&2
@@ -15,15 +18,15 @@ iface wlan0 inet manual
     wireless-mode ad-hoc
     # pre-select cell-id (faster connections)
     wireless-ap 90:43:ef:e0:be:e2
-    pre-up ifconfig \$IFACE up
-    post-down ifconfig \$IFACE down
+    address 169.254.$IP3.$IP4
+    netmask 255.255.0.0
 
 auto bat0
 iface bat0 inet static
     # decrease mtu to cope with batman overhead / raspi zero w limitations
     mtu 1468
     pre-up /usr/sbin/batctl if add wlan0
-    address $IP
+    address 10.254.$IP3.$IP4
     netmask 255.255.0.0
 EOF
 
