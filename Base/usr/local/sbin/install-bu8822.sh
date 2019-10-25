3fb6d09569004f32e9ae9be8f36a32cfb2155bf5
Jonas Höchst <git@jonashoechst.de>
2019-10-25 16:47:00 +0200

    skipping missing 8822bu drivers

--- Base/usr/local/sbin/install-bu8822.sh
+++ Base/usr/local/sbin/install-bu8822.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 DRIVERS_URL="http://downloads.fars-robotics.net/wifi-drivers/8822bu-drivers/"
 wget -O 8822bu-drivers.html $DRIVERS_URL
@@ -6,6 +7,11 @@ wget -O 8822bu-drivers.html $DRIVERS_URL
 for KERNEL in `ls /lib/modules`; do
     KERNEL_GREP=`echo $KERNEL | sed "s/+/-[0-9]*.tar.gz/"`
     TARFILE=`grep $KERNEL_GREP 8822bu-drivers.html | tail -n1 | cut -d\" -f8`
+    
+    if [ -z "$TARFILE" ]; then 
+        echo "Kernel $KERNEL: no driver available, skipping..."
+        continue
+    fi
 
     echo "Kernel $KERNEL: $TARFILE, downloading..."
     wget $DRIVERS_URL$TARFILE -O $TARFILE
