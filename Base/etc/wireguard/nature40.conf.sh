258a97e45139be06794e2cf36a8eb95d4c7feb5d
Jonas Höchst <git@jonashoechst.de>
2019-10-25 15:48:39 +0200

    using wireguard key from /boot partition, instead of generated key (implements #17)

--- Base/etc/wireguard/nature40.conf.sh
+++ Base/etc/wireguard/nature40.conf.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
+set -e
 
-ID=`cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2`
-
-PRIVKEY=`printf $ID$ID | base64`
+PRIVKEY=`cat "/boot/$HOSTNAME.wgkey"`
 PUBKEY=`echo $PRIVKEY | wg pubkey`
 
-IP=172.16.$((16#${ID:12:2})).$((16#${ID:14:2}))/16
+# compute IP from HOSTNAME remainder
+IP3=$((16#${HOSTNAME: -4: -2}))
+IP4=$((16#${HOSTNAME: -2}))
+IP="172.16.${IP3}.${IP4}/16"
 
 echo "Generating wireguard config..." 1>&2
 echo Public Key: $PUBKEY 1>&2
 echo Address:  $IP 1>&2
 
 cat <<EOF
-[Interface] # rpi: $ID
+[Interface] # $HOSTNAME
 Address = $IP
 PrivateKey = $PRIVKEY
 
