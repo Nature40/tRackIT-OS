26cbe87e6e783e825125b65880c6fd1b434e3cd5
Jonas Höchst <git@jonashoechst.de>
2019-12-13 15:07:46 +0100

    save config on logger start

--- RadioTracking/home/pi/signal_detect.sh
+++ RadioTracking/home/pi/signal_detect.sh
@@ -15,11 +15,25 @@ KEEPALIVE=300
 
 source "/boot/rtlsdr_signal_detect.conf"
 
-OUTFILE="/data/`hostname`/rtlsdr_signal_detect/`date +"%Y-%m-%dT%H%M%S"`_$1.csv"
-mkdir -p `dirname $OUTFILE`
+BASEFILE="/data/`hostname`/rtlsdr_signal_detect/`date +"%Y-%m-%dT%H%M%S"`"
+METAFILE="${BASEFILE}.conf"
+OUTFILE="${BASEFILE}_${1}.csv"
 
-rtl_sdr -d $1 -f $FREQUENCY -s $SAMPLERATE -g $GAIN - 2> $OUTFILE | \
-rtlsdr_signal_detect -s -t $THRESHOLD -r $SAMPLERATE -b ${FFT_BINS} -n ${FFT_SAMPLES} --ll ${DURATION_MIN} --lu ${DURATION_MAX} -k ${KEEPALIVE} >> $OUTFILE 2>&1
+tee ${METAFILE} <<EOF
+FREQUENCY=${FREQUENCY}
+SAMPLERATE=${SAMPLERATE}
+GAIN=${GAIN}
+THRESHOLD=${THRESHOLD}
+FFT_BINS=${FFT_BINS}
+FFT_SAMPLES=${FFT_SAMPLES}
+DURATION_MIN=${DURATION_MIN}
+DURATION_MAX=${DURATION_MAX}
+KEEPALIVE=${KEEPALIVE}
+EOF
 
-mv core `basename $OUTFILE`.core
+mkdir -p `dirname ${OUTFILE}`
+
+rtl_sdr -d ${1} -f ${FREQUENCY} -s ${SAMPLERATE} -g ${GAIN} - 2> ${OUTFILE} | \
+	rtlsdr_signal_detect -s -t ${THRESHOLD} -r ${SAMPLERATE} -b ${FFT_BINS} -n ${FFT_SAMPLES} --ll ${DURATION_MIN} --lu ${DURATION_MAX} -k ${KEEPALIVE} >> ${OUTFILE} 2>&1
+
 
