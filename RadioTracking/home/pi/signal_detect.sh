47462debca98c6db1b65c623c74e058fe572804e
Jonas Höchst <git@jonashoechst.de>
2020-01-22 14:28:49 +0100

    test stick before creating any files

--- RadioTracking/home/pi/signal_detect.sh
+++ RadioTracking/home/pi/signal_detect.sh
@@ -3,6 +3,7 @@
 set -e
 ulimit -c unlimited
 
+# default config
 FREQUENCY=150100001
 SAMPLERATE=250000
 GAIN=49
@@ -13,12 +14,21 @@ DURATION_MIN=0
 DURATION_MAX=1
 KEEPALIVE=300
 
+# additional configuration
 source "/boot/rtlsdr_signal_detect.conf"
 
+# test if device is available
+rtl_sdr -d ${1} -f ${FREQUENCY} -s ${SAMPLERATE} -g ${GAIN} -n1 /dev/null
+
+# generate output paths
 BASEFILE="/data/`hostname`/rtlsdr_signal_detect/`date +"%Y-%m-%dT%H%M%S"`"
 METAFILE="${BASEFILE}.conf"
 OUTFILE="${BASEFILE}_${1}.csv"
 
+# create output folder
+mkdir -p `dirname ${BASEFILE}`
+
+# save configuration
 tee ${METAFILE} <<EOF
 FREQUENCY=${FREQUENCY}
 SAMPLERATE=${SAMPLERATE}
@@ -31,9 +41,6 @@ DURATION_MAX=${DURATION_MAX}
 KEEPALIVE=${KEEPALIVE}
 EOF
 
-mkdir -p `dirname ${OUTFILE}`
-
+# run actual detection
 rtl_sdr -d ${1} -f ${FREQUENCY} -s ${SAMPLERATE} -g ${GAIN} - 2> ${OUTFILE} | \
 	rtlsdr_signal_detect -s -t ${THRESHOLD} -r ${SAMPLERATE} -b ${FFT_BINS} -n ${FFT_SAMPLES} --ll ${DURATION_MIN} --lu ${DURATION_MAX} -k ${KEEPALIVE} >> ${OUTFILE} 2>&1
-
-
